ecd68920be2ac87e647b82be383620fb5233da81
Leg3ndary <ben.zhou.2006@gmail.com>
2024-09-25 00:47:44 -0400

    Start adding LED's

--- src/main.ino
+++ src/main.ino
@@ -2,8 +2,18 @@
 Created by: Ben Zhou
 */
 
-void setup() {
+#define LED 18
+#define NUM_LEDS 60
 
+#include <FastLED.h>
+
+CRGB LEDs[NUM_LEDS];
+
+void setup() {
+    FastLED.addLeds<WS2812B, LED, GRB>(LEDs, NUM_LEDS);
+    FastLED.setBrightness(255);
+    FastLED.clear();
+    FastLED.show();
 }
 
 void loop() {
