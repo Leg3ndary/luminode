d1d367eabc5be9d6a2540dbc63515bb924c7e1c0
Leg3ndary <ben.zhou.2006@gmail.com>
2024-09-25 23:14:38 -0400

    Finished creating everything

--- src/main.ino
+++ src/main.ino
@@ -3,11 +3,26 @@ Created by: Ben Zhou
 */
 
 #define LED 18
-#define NUM_LEDS 60
-
+#define NUM_LEDS 259
 #include <FastLED.h>
 
 CRGB LEDs[NUM_LEDS];
+
+int step = 0;
+
+bool fadeLED() {
+    
+    if (step > 255) {
+        step = 0;
+        return false;
+    }
+    for (int i = 0; i < NUM_LEDS; i++) {
+        LEDs[i] = blend(CRGB(0, 128, 128), CRGB(0, 128, 128), step);
+    }
+    FastLED.show();
+    step += 1;
+    return true;
+}
 
 void setup() {
     FastLED.addLeds<WS2812B, LED, GRB>(LEDs, NUM_LEDS);
@@ -17,5 +32,6 @@ void setup() {
 }
 
 void loop() {
-
+    fadeLED();
+    delay(10);
 }
